4be8bd846be48897c7f23258bda18033cf286431
Gameplay07HUN <gameplay719@gmail.com>
2023-01-25 11:30:00 +0100

    Add project files.

---
+++ WindowsFormsAppLakoparkProjekt/HappyLiving.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppLakoparkProjekt
+{
+    internal class HappyLiving
+    {
+        public List<Lakopark> lakoparkok;
+
+        public HappyLiving(string fileName)
+        {
+            lakoparkok = new List<Lakopark>();
+        }
+    }
+}
---
+++ WindowsFormsAppLakoparkProjekt/Lakopark.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAppLakoparkProjekt
+{
+    internal class Lakopark
+    {
+        private string nev;
+        private int utcakSzama;
+        private int maxHazSzam;
+        private int[,] hazak;
+
+        public string Nev => nev;
+        public int UtcakSzama => utcakSzama;
+        public int MaxHazSzam => maxHazSzam;
+        public int[,] Hazak => hazak;
+
+        public Lakopark(string nev, int utcakSzama, int maxHazSzam, int[,] hazak)
+        {
+            this.nev = nev;
+            this.utcakSzama = utcakSzama;
+            this.maxHazSzam = maxHazSzam;
+            this.hazak = hazak;
+        }
+    }
+}
